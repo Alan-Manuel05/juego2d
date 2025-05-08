4fa9422d11c481d632c13475841fd6042f875f6f
Alan-Manuel05 <144375319+Alan-Manuel05@users.noreply.github.com>
2025-05-08 09:39:13 -0600

    Add files via upload

---
+++ Noche.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Noche" tilewidth="32" tileheight="32" tilecount="180" columns="18">
+ <image source="craftpix-net-846754-free-green-zone-tileset-pixel-art/2 Background/Night/5.png" width="576" height="324"/>
+</tileset>
---
+++ Noche3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Noche3" tilewidth="32" tileheight="32" tilecount="180" columns="18">
+ <image source="craftpix-net-846754-free-green-zone-tileset-pixel-art/2 Background/Night/1.png" width="576" height="324"/>
+</tileset>
